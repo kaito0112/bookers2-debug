c88d56f7280656b3af37f1058d3a2c953f215e8d
kaito0112 <123792716+kaito0112@users.noreply.github.com>
2023-03-03 12:43:06 +0000

    bookers2-debug-21

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
 
   has_many :books
   has_many :favorites, dependent: :destroy
+  has_many :favorited_book, through: :favorites, source: :book
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
 
